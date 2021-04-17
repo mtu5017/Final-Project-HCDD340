553a5d483f70b3c15964fa7a4c4adf47bc0337e8
mtu5017 <55293366+mtu5017@users.noreply.github.com>
2021-04-16 22:03:28 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is where the button for the chatbot will go. Once the user clicks the button, they will be asked to enter their name.</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Here are settings to change what the chatbot looks like visually.</Text>
+      <Text>TODO: Create button to toggle avatar</Text>
+      <Text>TODO: Create button to change username</Text>
+      <Text>TODO: Create accessibility button</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
